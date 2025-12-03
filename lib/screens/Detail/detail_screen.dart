2ba317563801dcf143f2334ec3709bf08e5f0f3d
Kamrul Hasan <kakamrul2000@gmail.com>
2025-12-04 00:38:56 +0600

    description_added

--- lib/screens/Detail/detail_screen.dart
+++ lib/screens/Detail/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_8/color_plate.dart';
 import 'package:flutter_application_8/models/product_model.dart';
+import 'package:flutter_application_8/screens/Detail/widget/description.dart';
 import 'package:flutter_application_8/screens/Detail/widget/detail_app_bar.dart';
 import 'package:flutter_application_8/screens/Detail/widget/image_slider.dart';
 import 'package:flutter_application_8/screens/Detail/widget/items_details.dart';
@@ -21,107 +22,111 @@ class _DetailScreenState extends State<DetailScreen> {
     return Scaffold(
       backgroundColor: kcontentColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            DetailAppBar(),
-            myImageSlider(
-              onChange: (index) {
-                setState(() {
-                  currentImage = index;
-                });
-              },
-              image: widget.product.image,
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                5,
-                (index) => AnimatedContainer(
-                  duration: Duration(microseconds: 300),
-                  width: currentImage == index ? 15 : 8,
-                  height: 8,
-                  margin: EdgeInsets.only(right: 3),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: currentImage == index
-                        ? Colors.black
-                        : Colors.transparent,
-                    border: Border.all(color: Colors.black),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DetailAppBar(),
+              myImageSlider(
+                onChange: (index) {
+                  setState(() {
+                    currentImage = index;
+                  });
+                },
+                image: widget.product.image,
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  5,
+                  (index) => AnimatedContainer(
+                    duration: Duration(microseconds: 300),
+                    width: currentImage == index ? 15 : 8,
+                    height: 8,
+                    margin: EdgeInsets.only(right: 3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: currentImage == index
+                          ? Colors.black
+                          : Colors.transparent,
+                      border: Border.all(color: Colors.black),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
-              ),
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
-                top: 20,
-                bottom: 100,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ItemsDetails(product: widget.product),
-                  SizedBox(height: 20),
-                  Text(
-                    'Color',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
                   ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: List.generate(
-                      widget.product.colors.length,
-                      (index) => GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            currentColor = index;
-                          });
-                        },
-                        child: AnimatedContainer(
-                          duration: Duration(milliseconds: 300),
-                          width: 35,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: currentColor == index
-                                ? Colors.white
-                                : widget.product.colors[index],
-                            border: currentColor == index
-                                ? Border.all(
-                                    color: widget.product.colors[index],
-                                  )
-                                : null,
-                          ),
-                          padding: currentColor == index
-                              ?  EdgeInsets.all(2)
-                              : null,
-                          margin: EdgeInsets.only(right: 10),
-                          child: Container(
+                ),
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 20,
+                  bottom: 100,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ItemsDetails(product: widget.product),
+                    SizedBox(height: 20),
+                    Text(
+                      'Color',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: List.generate(
+                        widget.product.colors.length,
+                        (index) => GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              currentColor = index;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: Duration(milliseconds: 300),
                             width: 35,
                             height: 35,
                             decoration: BoxDecoration(
-                              color: widget.product.colors[index],
                               shape: BoxShape.circle,
+                              color: currentColor == index
+                                  ? Colors.white
+                                  : widget.product.colors[index],
+                              border: currentColor == index
+                                  ? Border.all(
+                                      color: widget.product.colors[index],
+                                    )
+                                  : null,
+                            ),
+                            padding: currentColor == index
+                                ? EdgeInsets.all(2)
+                                : null,
+                            margin: EdgeInsets.only(right: 10),
+                            child: Container(
+                              width: 35,
+                              height: 35,
+                              decoration: BoxDecoration(
+                                color: widget.product.colors[index],
+                                shape: BoxShape.circle,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),const SizedBox(height: 20,),
-                ],
+                    const SizedBox(height: 25),
+                    Description(description: widget.product.description)
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/Detail/widget/description.dart
+++ lib/screens/Detail/widget/description.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_8/color_plate.dart';
 
 class Description extends StatelessWidget {
-  final String text;
-  const Description({super.key, required this.text});
+  final String description;
+  const Description({super.key, required this.description});
 
   @override
   Widget build(BuildContext context) {
@@ -11,25 +11,46 @@ class Description extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
               width: 120,
               height: 40,
               decoration: BoxDecoration(
                 color: kprimaryColor,
+
                 borderRadius: BorderRadius.circular(20),
               ),
               alignment: Alignment.center,
               child: Text(
                 'Description',
                 style: TextStyle(
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
               ),
             ),
+            Text(
+              'Specifications',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Reviews',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
           ],
         ),
+        SizedBox(height: 20),
+        Text(description, style: TextStyle(fontSize: 16, color: Colors.grey)),
       ],
     );
   }
